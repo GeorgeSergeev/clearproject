48e2b6df2f5b149ec2b3aaffd03e8f2c4dd48108
George Sergeev <pu88@mail.ru>
2013-12-09 18:23:37 +0200

    start

---
+++ src/main/java/ua/softserve/clearproject/services/StudentsService.java
@@ -0,0 +1,9 @@
+package ua.softserve.clearproject.services;
+
+import java.util.List;
+
+import ua.softserve.clearproject.student.Student;
+
+public interface StudentsService {
+	List <Student> loadStudentList();
+}
